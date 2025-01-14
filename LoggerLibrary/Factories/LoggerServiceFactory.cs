92f5df5d90818eb913c49a7d5a30c952de0b6332
Manuel Moran <manuel.moran@arrivia.com>
2025-01-14 16:18:37 -0700

    ILoggerServiceFactory and ILoggerService implemented using Serilog.

---
+++ LoggerLibrary/Factories/LoggerServiceFactory.cs
@@ -0,0 +1,37 @@
+﻿using ConfigurationLibrary.Interfaces.Factories;
+using LoggerLibrary.Interfaces.Factories;
+using LoggerLibrary.Interfaces.Services;
+using LoggerLibrary.Services;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace LoggerLibrary.Factories;
+
+public class LoggerServiceFactory : ILoggerServiceFactory
+{
+	private const string _serilogConfigKey = "Serilog";
+
+	private readonly IConfigurationServiceFactory _configurationServiceFactory;
+
+	public LoggerServiceFactory(IConfigurationServiceFactory configurationServiceFactory)
+	{
+		_configurationServiceFactory = configurationServiceFactory;
+	}
+
+	public ILoggerService CreateSerilogLoggerService(string settingsFilePath, string settingsFileName, string filePath, string fileName)
+	{
+		ArgumentException.ThrowIfNullOrWhiteSpace(settingsFilePath);
+		ArgumentException.ThrowIfNullOrWhiteSpace(settingsFileName);
+		ArgumentException.ThrowIfNullOrWhiteSpace(filePath);
+		ArgumentException.ThrowIfNullOrWhiteSpace(fileName);
+
+		var configuration = _configurationServiceFactory
+			.CreateConfigurationService(settingsFilePath, settingsFileName)
+			.GetConfigurationSection<IConfiguration>(_serilogConfigKey);
+
+		var fullFilePath = Path.Combine(filePath, fileName);
+
+		return new SerilogLoggerService(configuration, fullFilePath);
+	}
+}
